06a2f80be2156a3caed8402d923404941d032859
Aseelsamer <assoul_08@hotmail.com>
2020-12-29 19:13:32 +0200

    test

---
+++ __test__/array-shift-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const arrayShift= require('../array-reverse/array-reverse');
+
+describe('to pass the solution and return the array reversed',()=>{
+    it('the test passs will return reverse array',()=>{
+        expect(arrayShift([2, 4, 6, 8],5).toEqual([2,4,5,6,8]))
+    })
+});
+
+describe('to pass the solution and return the array reversed',()=>{
+    it('the test passs will return reverse array',()=>{
+        expect(arrayShift([4, 8, 15, 23, 42],16).toEqual([4,8,15,16,23,42]))
+    })
+});
